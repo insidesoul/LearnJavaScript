61e72fa358ee9e29e39b67ba0555a41db8a8c5cb
Vladimir Dzyuba <97099739+insidesoul@users.noreply.github.com>
2022-06-04 15:22:48 +0300

    couse#2

--- app.js
+++ app.js
@@ -848,3 +848,23 @@ function halvingSum(n) {
     }
     return s_sum;  
 }
+
+//-----------------------------------------------------------------
+
+function grabDoll(dolls){
+  var bag=[];
+  for(let i = 0 ; i < dolls.length; i++){
+    if (bag.length === 3) {
+      break
+    }
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll'){
+      bag.push(dolls[i])
+    } else {
+      continue
+    }
+  }
+  
+  return bag;
+}
+
+//------------------------------------------------------------
