16b5eb82ec447b031636513f055ff00092731b7f
Vladimir Dzyuba <97099739+insidesoul@users.noreply.github.com>
2022-06-14 09:51:10 +0300

    course#2

--- app.js
+++ app.js
@@ -1286,3 +1286,16 @@ function expressionMatter(a, b, c) {
 }
 
 //---------------------------------------------------------
+
+function  yourFutureCareer(){
+  var career = Math.random()
+    if (career <= 0.32) {
+      return "FrontEnd Developer"
+    } else if (career <= 0.65) {
+      return "BackEnd Developer"
+    } else {
+      return 'Full-Stack Developer'
+    }
+  }
+
+//-------------------------------------------------------
