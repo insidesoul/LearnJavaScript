e812930634c6a2d52d6191a01d4dff8a24b0c946
Vladimir Dzyuba <vovadzyuba14@gmail.com>
2022-07-24 18:53:27 +0300

    course#2

--- app.js
+++ app.js
@@ -4123,3 +4123,32 @@ Array.prototype.inRange = function(min,max){
     return this.filter(el => (el > min-1 && el < max+1) && Number.isInteger(el))
 }
 
+Boolean.prototype.toString = function() {
+  return this + ''
+}
+Number.prototype.toString = function() {
+  return this + ''
+}
+Array.prototype.toString = function() {
+  return '['+this.join(',')+']'
+}
+
+//--------------------------------------------------
+
+// Для преобразования строки к целому числу используется метод Number.parseInt(string) или функция parseInt(string).
+
+// Принципы их работы идентичны:
+
+// имеют два параметра: string и radix. Radix – основание системы счисления (число от 2 до 36), которое считается необязательным, но на практике лучше всегда его указывать;
+// пробелы в начале и конце строки не учитываются;
+// если значение, переданное в строку не является string, то неявно преобразовывается;
+// возвращаемое значение – целое число или NaN, если первый символ строки не удалось преобразовать в число.
+// console.log(parseInt('56.5ab', 10)); // 56
+// console.log(parseInt('     5abc   ', 16)); // 23228
+// console.log(parseInt(56.15, 10)); // 56
+// console.log(Number.parseInt('-56.15', 10)); // -56
+// console.log(Number.parseInt(100, 2)); // 4 
+// console.log(Number.parseInt('$56abc', 16)); // NaN
+// console.log(parseInt('abc5.6')); // NaN
+
+//--------------------------------------------------
